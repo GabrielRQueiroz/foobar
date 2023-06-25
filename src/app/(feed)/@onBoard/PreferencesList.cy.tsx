e9ea6cf3377773494d5bd56dc225d34250660354
GabrielRQueiroz <gabrielr.queiroz@gmail.com>
2023-06-25 15:06:24 -0300

    🧪 test: Cards de preferência

--- src/app/(feed)/@onBoard/PreferencesList.cy.tsx
+++ src/app/(feed)/@onBoard/PreferencesList.cy.tsx
@@ -7,6 +7,13 @@ describe('<PreferencesList />', () => {
 
 	it('renders rows with selectable preferences cards properly', () => {
 		cy.mount(<PreferencesList />)
-		cy.get('[data-cy=preference-card]').should('have.class', 'card').and("have.attr", 'key').and('have.lengthOf.within', 6, 32)
+
+		cy.get('[data-cy="preferences-list"]').children().should('have.lengthOf.within', 6, 32)
+
+		cy.get('[data-cy="preference-card"]').should('have.class', 'card').and("have.class", "aspect-square")
+
+		cy.get('[data-cy="preference-card"]').click()
+
+		cy.get('[data-cy="preference-card-checked"]').should('be.visible')
 	})
 })
