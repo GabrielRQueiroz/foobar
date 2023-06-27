90d152ba71e40014db9a6edc1f723e5cb167e8ab
PedroLife01 <pedrosardenberg@hotmail.com>
2023-06-27 16:32:43 -0300

    🌈 style: Responsive Form

--- src/app/registro/page.tsx
+++ src/app/registro/page.tsx
@@ -2,28 +2,17 @@ import FormRegistro from "@/components/pages/Registro/FormRegistro"
 import Image from "next/image"
 
 const RegistroPage = () => {
-    return(
-        <div className="flex justify-between w-full h-screen bg-white">
-                <div className="m-auto">
-                <Image
-                    src="/Emilia-Registro.webp"
-                    width={450}
-                    height={570}
-                    alt="Subaru Login"
-                    />
-                </div>
-                <div className="m-auto">
-                    <Image
-                        src="/Rematch-logo.webp"
-                        width={350}
-                        height={250}
-                        alt="Subaru Login"
-                        />
-                    <FormRegistro/>
-                </div>
-        </div>
-    
-    )
+    return (
+			<div className="flex h-screen w-full justify-between bg-white">
+				<div className="m-auto hidden md:inline">
+					<Image src="/Emilia-Registro.webp" width={450} height={570} alt="Subaru Login" />
+				</div>
+				<div className="m-auto">
+					<Image src="/Rematch-logo.webp" width={350} height={250} alt="Subaru Login" />
+					<FormRegistro />
+				</div>
+			</div>
+		)
 }
 
 export default RegistroPage
