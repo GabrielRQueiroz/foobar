29980b94e5a5e09a1816b9c7768c26f00d2583c8
GabrielRQueiroz <gabrielr.queiroz@gmail.com>
2023-07-05 17:03:41 -0300

    Fix: foobar :)

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -4,7 +4,7 @@ import { rest } from 'msw'
 
 export const handlers = [
 	rest.get(apiEndpoints.GET_PREFERENCES, (req, res, ctx) => {
-		const limit = Number(req.url.searchParams.get('limit'))
+		// const limit = Number(req.url.searchParams.get('limit'))
 
 		return res(ctx.status(200), ctx.json([...preferencesData, ...preferencesData] as any[]))
 	}),
