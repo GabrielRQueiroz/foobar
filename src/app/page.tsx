ec72692eb39b3d0ba0b1aa05cec2de2e3d857ba5
Gabriel Henrique <henriqueb.26@gmail.com>
2023-07-08 20:20:14 -0300

    🌈 style: home theme switch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,22 @@ const LandingPage = () => {
 					<div className="card m-auto flex h-5/6 w-11/12 flex-row rounded-3xl bg-base-100 text-base-content">
 						<div className="flex w-7/12 flex-col justify-between py-8 pl-14 ">
 							<div className="flex flex-col justify-start gap-y-8">
-								<Image src="/Rematch-logo.webp" width={160} height={100} alt="Subaru Login" />
+								<Image
+									priority
+									className="hidden [[data-theme=fantasy]_&]:block"
+									src="/Rematch-logo.webp"
+									width={160}
+									height={100}
+									alt="Logo Form"
+								/>
+								<Image
+									priority
+									className="hidden [[data-theme=dark]_&]:block "
+									src="/Rematch-lg-white.webp"
+									width={160}
+									height={100}
+									alt="Logo Form"
+								/>
 								<h1 className="w-8/12 font-sans text-7xl font-semibold">Ache sua história</h1>
 								<p className="w-10/12">
 									Explore novas e emocionantes experiências cinematográficas, televisivas e literárias com nosso site de
@@ -22,7 +37,20 @@ const LandingPage = () => {
 										<Link href="/registro">Cadastre-se</Link>
 									</button>
 									<div className="w-auto">
-										<Image src="/icones.png" width={120} height={0} alt="icons" />
+										<Image
+											className="hidden [[data-theme=fantasy]_&]:block"
+											src="/icones.png"
+											width={120}
+											height={0}
+											alt="icons"
+										/>
+										<Image
+											className="hidden [[data-theme=dark]_&]:block"
+											src="/iconesbranco.png"
+											width={120}
+											height={0}
+											alt="icons"
+										/>
 									</div>
 									<p className="font-semibold">+ de 1 milhão de séries, livros e filmes onde e quando você quiser!</p>
 								</div>
@@ -33,12 +61,7 @@ const LandingPage = () => {
 						</div>
 						<div className="flex w-5/12 pl-0">
 							<div className="relative h-full w-full">
-								<Image
-									className="rounded-2xl object-cover"
-									fill
-									src="/Emilia-LandingPage.jpg"
-									alt="Emilia Landing"
-								/>
+								<Image className="rounded-2xl object-cover" fill src="/Emilia-LandingPage.jpg" alt="Emilia Landing" />
 							</div>
 						</div>
 					</div>
@@ -48,4 +71,4 @@ const LandingPage = () => {
 	)
 }
 
-export default LandingPage;
+export default LandingPage
