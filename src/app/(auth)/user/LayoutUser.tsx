d011a36ee00278a8049f401d60a6e0df938b2107
Júlia Baba <julia.baba@grupoorion.eng.br>
2023-07-09 14:54:43 -0300

    ✨ component list of friends

--- src/app/(auth)/user/LayoutUser.tsx
+++ src/app/(auth)/user/LayoutUser.tsx
@@ -4,10 +4,10 @@ export const LayoutUser = () => {
 	return (
 		<>
 			<div className="flex h-full w-full flex-col justify-evenly gap-2">
-				<div className="flex flex-col ">
+				<div className="flex flex-col">
 					<Usercard nome="Shaolin" email="shaolinmatadordeporco@hotmail.com" numeroMatches={20} />
 				</div>
-				<div className="flex h-3/5 flex-col ">
+				<div className="flex h-3/5 flex-col">
 					<PreferencesCard
 						preferences={[
 							{ title: 'O alto da compadecida', gender: 'Comedia', image: '/user_profile.jpg' },
--- src/app/(auth)/user/page.tsx
+++ src/app/(auth)/user/page.tsx
@@ -1,12 +1,18 @@
 import { LayoutUser } from './LayoutUser'
-
+import FriendsList from '@/components/FriendsList/FriendsList'
 const UserPage = () => {
 	return (
 		<>
-			<div className="flex h-screen w-full border-8 bg-base-100 p-8 text-base-content">
-				<LayoutUser />
-				<div className=" phone-1 artboard artboard-horizontal text-base-content">teste</div>
-				{/* <Image src="/background-LandingPage.jpg" width={400} height={0} alt="background" /> */}
+			<div className="flex h-screen w-full gap-6 bg-base-100 p-8 text-base-content">
+				<div className="flex w-8/12">
+					<LayoutUser />
+				</div>
+				<div className="flex h-full w-4/12 rounded-b-xl  border-x-2 border-b-2 text-base-content">
+					<FriendsList
+						numberOfFriends={12}
+						listOfFriends={['Juninho do Brás', 'Marquinhos pastel', 'Tião Carreiro', 'Xandinho do Samba']}
+					/>
+				</div>
 			</div>
 		</>
 	)
